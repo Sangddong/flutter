ef02bcefd22c78aca608cebbe03c9b96cb893fbe
sangddong <hehe74029@gmail.com>
2025-12-18 20:27:11 +0900

    stless lifecycle

---
+++ statelesswidget_lifecycle/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:statelesswidget_lifecycle/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
---
+++ statelesswidget_lifecycle/lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CodeFactoryWidget()
+    );
+  }
+}
+
+class CodeFactoryWidget extends StatelessWidget {
+  CodeFactoryWidget({super.key}){
+    print("--- constructor ---");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("--- build ---");
+    
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: Colors.red
+    );
+  }
+}
