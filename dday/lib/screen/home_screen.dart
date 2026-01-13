a610a78fcf19c14ab8181722684f219ca85691fa
sangddong <hehe74029@gmail.com>
2026-01-13 18:19:01 +0900

    setState

--- dday/lib/screen/home_screen.dart
+++ dday/lib/screen/home_screen.dart
@@ -19,11 +19,19 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _Top extends StatelessWidget {
+class _Top extends StatefulWidget {
   const _Top({super.key});
 
   @override
+  State<_Top> createState() => _TopState();
+}
+
+class _TopState extends State<_Top> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     final textTheme = Theme
         .of(context)
         .textTheme;
@@ -40,7 +48,7 @@ class _Top extends StatelessWidget {
             style: textTheme.bodyMedium,
           ),
           Text(
-            '2025.11.24',
+            '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
             style: textTheme.bodySmall,
           ),
           IconButton(
@@ -59,7 +67,9 @@ class _Top extends StatelessWidget {
                       child: CupertinoDatePicker(
                         mode: CupertinoDatePickerMode.date,
                         onDateTimeChanged: (DateTime date) {
-                          print(date);
+                          setState(() {
+                            selectedDate = date;
+                          });
                         },
                         dateOrder: DatePickerDateOrder.ymd,
                       ),
@@ -70,7 +80,7 @@ class _Top extends StatelessWidget {
             icon: Icon(Icons.favorite),
           ),
           Text(
-            'D + 1',
+            'D + ${now.difference(selectedDate).inDays + 1}',
             style: textTheme.displayLarge,
           ),
         ],
